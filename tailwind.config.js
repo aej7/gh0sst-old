a9bb6f53dffd3251e6b0e130cf80011ea9e753ba
alex s <alex.svorada@skiff.com>
2023-07-06 15:21:15 +0200

    feat: add better image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        montserrat: ['Montserrat', 'sans-serif'],
-      },
-      backgroundImage: {
-        skulls: "url('/src/assets/bg-image.jpg')",
-      },
-    },
-  },
-  plugins: [],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				montserrat: ['Montserrat', 'sans-serif'],
+			},
+			backgroundImage: {
+				image: "url('/src/assets/bg-image1.jpg')",
+			},
+		},
+	},
+	plugins: [],
 }
