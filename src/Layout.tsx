13c583411e010d3e643739a292fc047e1f2356ed
aej7 <alex.svorada@skiff.com>
2023-07-08 00:24:29 +0200

    feat: improved naming

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,23 +1,23 @@
 import Header from './components/Header/Header.tsx'
 import Content from './components/Content/Content.tsx'
 import { useState } from 'react'
-import { ContentVisibleContext } from './context/ContentVisibleContext.tsx'
+import { ContentVisibilityContext } from './context/ContentVisibilityContext.tsx'
 import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
-	const [isContentVisible, setIsContentVisible] = useState(true)
+	const [isContentVisibility, setIsContentVisibility] = useState(true)
 
 	return (
 		<main
 			className='w-screen h-screen bg-image grayscale bg-cover overflow-auto flex flex-col gap-14
       md:gap-16 lg:gap-24 xl:gap-36 font-montserrat font-light antialiased text-white'
 		>
-			<ContentVisibleContext.Provider
-				value={{ isContentVisible, setIsContentVisible }}
+			<ContentVisibilityContext.Provider
+				value={{ isContentVisibility, setIsContentVisibility }}
 			>
 				<Header />
-				<Content>{isContentVisible ? <Outlet /> : <></>}</Content>
-			</ContentVisibleContext.Provider>
+				<Content>{isContentVisibility && <Outlet />}</Content>
+			</ContentVisibilityContext.Provider>
 		</main>
 	)
 }
