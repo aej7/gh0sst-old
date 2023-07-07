0f7e221f04a90b1eda4477eaa541ed64d725b6eb
aej7 <alex.svorada@skiff.com>
2023-07-07 11:36:16 +0200

    feat: format text to the left for accessibility

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,8 +1,5 @@
 const Text = ({ children }: React.PropsWithChildren) => {
-	return (
-		<p className='text-base text-center tracking-widest lowercase'>{children}</p>
-	)
+	return <p className='text-base text-left tracking-widest lowercase'>{children}</p>
 }
 
 export default Text
-
